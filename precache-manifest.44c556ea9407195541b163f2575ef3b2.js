f2b3a051443e0f388d91b3d4a9466cb0f39f0a1f
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-06-11 19:15:40 -0500

    Updates

--- precache-manifest.44c556ea9407195541b163f2575ef3b2.js
+++ precache-manifest.44c556ea9407195541b163f2575ef3b2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea8d1d8e0e38d697e316f25a123c0927",
+    "revision": "3e465a3c2209d712a5c56ef21f9b31e3",
     "url": "/stars-wars/index.html"
   },
   {
-    "revision": "7f4f9d45ca99c80c51ea",
+    "revision": "b4fbd0564542b1519d3a",
     "url": "/stars-wars/static/css/main.84c495a8.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/stars-wars/static/js/2.fd054270.chunk.js"
   },
   {
-    "revision": "7f4f9d45ca99c80c51ea",
-    "url": "/stars-wars/static/js/main.84519dd9.chunk.js"
+    "revision": "b4fbd0564542b1519d3a",
+    "url": "/stars-wars/static/js/main.b14a0f24.chunk.js"
   },
   {
     "revision": "31a3d5eff80259d21d1e",
